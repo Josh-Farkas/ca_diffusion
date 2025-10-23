0fe653c4bb6f8d4db647c08da6bb05fb0034d0e6
Nora Osmanova <106052102+noraosman@users.noreply.github.com>
2025-10-23 12:43:03 -0700

    Finished applyDiffusionExtended

--- applyDiffusionExtended.py
+++ applyDiffusionExtended.py
@@ -1,2 +1,16 @@
 import numpy as N
 
+import Diffusion
+
+r = 0.1
+def applyDiffusionExtended(latExt):
+   newLatExt = latExt.copy()
+   m, n = latExt.shape
+   
+   for i in range(1, m-1):
+        for j in range(1, n-1):
+            newLatExt[i,j] = Diffusion(r, latExt[i,j], latExt[i-1,j], latExt[i-1,j+1], latExt[i,j+1], latExt[i+1,j+1], latExt[i+1,j], latExt[i+1,j-1], latExt[i,j-1], latExt[i-1,j-11])
+    
+   return newLatExt
+
+

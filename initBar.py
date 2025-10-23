3751d70b69f77c5afa5de3491ebef1adf78bd5c7
Farkler <59152302+Farkler0@users.noreply.github.com>
2025-10-23 12:18:15 -0700

    create initBar

--- initBar.py
+++ initBar.py
@@ -1,1 +1,17 @@
+import numpy as np
 
+# cold < ambient < hot
+AMBIENT = 5
+HOT = 10
+COLD = 1
+
+def appyHotCold(bar, hotSites, coldSites):
+    for hot in hotSites:
+        bar[*hot] = HOT
+    for cold in coldSites:
+        bar[*cold] = COLD
+    return bar
+
+def initBar(m, n, hotSites, coldSites):
+    ambientBar = np.full((m, n), AMBIENT)
+    return appyHotCold(ambientBar, hotSites, coldSites)

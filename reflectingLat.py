db6b845a8b82a29888ba84d0b9e1622b43257c5f
Kyle <kylewilliams342@gmail.com>
2025-11-05 20:04:10 -0800

    Implemented reflectingLat concatenate logic.

--- reflectingLat.py
+++ reflectingLat.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 def reflectingLat(lat):
-    
+    firstCol = lat[:, 0].reshape(-1, 1)
+    lastCol = lat[:, -1].reshape(-1, 1)
+
+    latEW = np.concatenate((firstCol, lat, lastCol), axis=1)
+
+    firstRow = latEW[0, :].reshape(1, -1)
+    lastRow = latEW[-1, :].reshape(1, -1)
+
+    latNWSE = np.concatenate((firstRow, latEW, lastRow), axis=0)
+
+    return latNWSE
